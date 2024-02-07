68dbb6cc7c20cc4be63b32e8ec397583ad97b8b3
Omar Ibraheem <ibraheemomar84@yahoo.com>
2024-02-07 12:59:15 -0500

    finished the first 2 verbal question

---
+++ solutions.js
@@ -0,0 +1,5 @@
+// Verbal questions
+// What is the difference between a parameter and an argument?
+    /// A parameter is one of the a variables in s function, and when the function is called the arguments are the data you pass into the functions parameters 
+// Within a function, what is the difference between return and console.log?
+    /// console.log() is a function that logs arguments to the console and return is a keyword that is used to end a function 
