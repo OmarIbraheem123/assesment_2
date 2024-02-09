aca8b2a5bf91e1fe2e854cb8f5311c8744981a05
Omar Ibraheem <ibraheemomar84@yahoo.com>
2024-02-08 21:25:20 -0500

    finished palindrome

--- solutions.js
+++ solutions.js
@@ -2,4 +2,26 @@
 // What is the difference between a parameter and an argument?
     /// A parameter is one of the a variables in s function, and when the function is called the arguments are the data you pass into the functions parameters 
 // Within a function, what is the difference between return and console.log?
-    /// console.log() is a function that logs arguments to the console and return is a keyword that is used to end a function 
+    /// console.log() is a function that logs arguments to the console and return is a keyword that is used to end a function \
+
+
+
+
+
+    ///Palindrome
+function palindrome(str) {
+    const string = str.toLowerCase()
+    const splitString = string.split().reverse().join()
+    if (string !== splitString) {
+        return false
+    }
+}
+console.log(palindrome(level))
+
+
+
+/// sum Array
+
+// function sumArray (sum) {
+//     const 
+// }
